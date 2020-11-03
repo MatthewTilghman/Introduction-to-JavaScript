eea9a849d81de21bc5e093ecd7761a43b15345c2
Matt Tilghman <sole@Soles-MacBook-Pro.local>
2020-11-03 18:37:34 -0500

    all tests passing mvp met

--- index.js
+++ index.js
@@ -266,10 +266,20 @@ Using the grade function below do the following:
    below 60 = F
 */
   
-function grade(/*add your code here*/){
-    /*add your code here*/
+function grade(score){
+    if (score >= 90 && score <= 100){
+      return "you got an A";
+    } else if (score >= 80 && score <= 89){
+      return "you got a B";
+    } else if (score >= 70 && score <= 79){
+      return "you got a C";
+    } else if (score >= 60 && score <= 69){
+      return "you got a D";
+    } else if (score < 60){
+      return "you got a F";
+    }
   }
-  
+  console.log(grade());
   
   
   
